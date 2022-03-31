aafaebaa14629a5d51097b08dc1c1b73e5fad726
Rafael Silva <rafaelrst15@gmail.com>
2022-03-30 23:49:57 -0300

    Feito Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './navbar.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  NavbarText,
+  DropdownItem } from 'reactstrap';
+
+const Navigator = () => {
+
+  const [click, setClick] = useState(false)
+
+  function noRefCheck(){
+    setClick((click) => !click)
+  }
+
+    return(
+      <div>
+      <Navbar
+        color="success"
+        dark
+        expand="lg"
+        fixed=""
+        full
+        light
+      >
+        <NavbarBrand href="/">
+          RGeasy
+        </NavbarBrand>
+        <NavbarToggler onClick={noRefCheck} />
+        <Collapse navbar isOpen={click}>
+          <Nav
+            className="me-auto"
+            navbar
+          >
+            <NavItem className='item'>
+              <NavLink href="#">
+                RGeasy Home
+              </NavLink>
+            </NavItem>
+            <NavItem className='item'>
+              <NavLink href="#">
+                Espécies
+              </NavLink>
+            </NavItem>
+            <NavItem className='item'>
+              <NavLink href="#">
+                Registrar Espécies
+              </NavLink>
+            </NavItem>
+            <NavItem className='item'>
+              <NavLink href="#">
+                Sobre Nós
+              </NavLink>
+            </NavItem>
+            <NavItem className='item'>
+              <NavLink href="#">
+                Contato
+              </NavLink>
+            </NavItem>
+            <NavItem className='item'>
+              <NavLink href="#">
+                Entrar
+              </NavLink>
+            </NavItem>
+          </Nav>
+            <UncontrolledDropdown
+              inNavbar
+            >
+              <DropdownToggle
+                caret
+                nav
+              >
+                Português
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  English
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          <NavbarText className='item'>
+            UFT
+          </NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+    );
+}
+
+export default Navigator;
