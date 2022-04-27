21ef19a9e1e4d82a73a2833e937a73fcd3b22dbe
Rafael Silva <rafaelrst15@gmail.com>
2022-04-27 01:35:44 -0300

    Estilizando telas de login e cadastro

--- src/screens/Cadastro/index.js
+++ src/screens/Cadastro/index.js
@@ -3,57 +3,60 @@ import './cadastro.css';
 import Navigator from "../../components/Navbar";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-const Cadastro = () =>{
+const Cadastro = () =>{ 
     return(
         <div className="back-cad">
             <Navigator/>
-        <div className="container">
-            <div style={{textAlign: "center", marginTop: 50}}>
-                <h3>Cadastrar</h3>
-            </div>
-            <div className="margem">
-            <Form inline>
-            <FormGroup>
-                <Label>Nome</Label>
-                <Input
-                id="exampleEmail"
-                name="name"
-                placeholder="Nome"
-                type="text"
-                />
-            </FormGroup>
-            <FormGroup>
-                <Label>Email</Label>
-                <Input
-                id="exampleEmail"
-                name="email"
-                placeholder="Email"
-                type="email"
-                />
-            </FormGroup>
-            <FormGroup>
-                <Label>Senha</Label>
-                <Input
-                id="examplePassword"
-                name="Senha"
-                placeholder="Senha"
-                type="password"
-                />
-            </FormGroup>
-            <FormGroup>
-                <Label>Confirmar senha</Label>
-                <Input
-                id="examplePassword"
-                name="password"
-                placeholder="Confirmar senha"
-                type="password"
-                />
-            </FormGroup>
-            <Button className="large">Cadastre-se</Button>
-            <div style={{textAlign: "center", marginTop: 10}}>
-                <a href="/entrar">Login</a>
-            </div>
-            </Form>
+            <div className="fundo">
+            <div className="container">
+                <div className="caixa2 margem">
+                <div style={{textAlign: "center"}}>
+                    <h3>Cadastrar</h3>
+                </div>
+                <Form inline>
+                <FormGroup>
+                    <Label>Nome</Label>
+                    <Input
+                    id="exampleEmail"
+                    name="name"
+                    placeholder="Nome"
+                    type="text"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Email</Label>
+                    <Input
+                    id="exampleEmail"
+                    name="email"
+                    placeholder="Email"
+                    type="email"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Senha</Label>
+                    <Input
+                    id="examplePassword"
+                    name="Senha"
+                    placeholder="Senha"
+                    type="password"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Confirmar senha</Label>
+                    <Input
+                    id="examplePassword"
+                    name="password"
+                    placeholder="Confirmar senha"
+                    type="password"
+                    />
+                </FormGroup>
+                <Button className="large">Cadastre-se</Button>
+                <div style={{textAlign: "center", marginTop: 10}}>
+                    <text>Já possui uma conta? </text>
+                    <a href="/entrar">Login</a>
+                </div>
+                </Form>
+                </div>
             </div>
             </div>
         </div>
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -7,36 +7,38 @@ const Login = () =>{
     return(
         <div className="back-login">
             <Navigator/>
-        <div className="container">
-            <div style={{textAlign: "center", marginTop: 50}}>
-                <h3>Login</h3>
-            </div>
-            <div className="margem">
-            <Form inline>
-            <FormGroup>
-                <Label>Email</Label>
-                <Input
-                id="exampleEmail"
-                name="email"
-                placeholder="Email"
-                type="email"
-                />
-            </FormGroup>
-            <FormGroup>
-                <Label>Password</Label>
-                <Input
-                id="examplePassword"
-                name="password"
-                placeholder="Password"
-                type="password"
-                />
-            </FormGroup>
-            <p>Esqueceu sua Senha? <a href="#">Recuperar Senha</a></p>
-            <Button className="large">Entrar</Button>
-            <div style={{textAlign: "center", marginTop: 10}}>
-                <a href="/cadastro">Cadastre-se</a>
-            </div>
-            </Form>
+            <div className="fundo">
+            <div className="container">
+                <div className="caixa2 margem">
+                <div style={{textAlign: "center"}}>
+                    <h3>Login</h3>
+                </div>
+                <Form inline>
+                <FormGroup>
+                    <Label>Email</Label>
+                    <Input
+                    id="exampleEmail"
+                    name="email"
+                    placeholder="Email"
+                    type="email"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Password</Label>
+                    <Input
+                    id="examplePassword"
+                    name="password"
+                    placeholder="Password"
+                    type="password"
+                    />
+                </FormGroup>
+                <p>Esqueceu sua Senha? <a href="#">Recuperar Senha</a></p>
+                <Button className="large">Entrar</Button>
+                <div style={{textAlign: "center", marginTop: 10}}>
+                    <a href="/cadastro">Cadastre-se</a>
+                </div>
+                </Form>
+                </div>
             </div>
             </div>
         </div>
